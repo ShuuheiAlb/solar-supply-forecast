ddbbff5f147925597f983ca1f718e5ca752d039d
ShuuheiAlb <eagleluph@gmail.com>
2024-06-08 18:05:35 +1000

    ah

--- lib.py
+++ lib.py
@@ -7,7 +7,7 @@
 etl_out_path = "data/etl_out.csv"
 etl_out_db_path = "data/etl_out.db" #soon sqlite3
 station_path = "data/station.csv"
-model_out_path = "data/model_out.csv"
+model_out_path = "data/model.pkl"
 
 # Forecast horizon
 h = 7
